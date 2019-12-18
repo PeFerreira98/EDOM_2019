c76ba7b3ce8e7cd06562319b0f3b4e7521480e9e
Vitor Melo <vitor3melo@outlook.pt>
2019-12-18 19:51:28 +0000

    domain commit

--- project-assignment5/mei-isep-pt.isep.edom.project1920/pt.isep.edom.project.c3.mm.domain.edit/src-gen/pt/isep/edom/project/c3/mm/domain/provider/ReferenceItemProvider.java
+++ project-assignment5/mei-isep-pt.isep.edom.project1920/pt.isep.edom.project.c3.mm.domain.edit/src-gen/pt/isep/edom/project/c3/mm/domain/provider/ReferenceItemProvider.java
@@ -63,22 +63,6 @@ public class ReferenceItemProvider extends ItemProviderAdapter implements IEditi
 	}
 
 	/**
-	 * This adds a property descriptor for the Name feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected void addNamePropertyDescriptor(Object object) {
-		itemPropertyDescriptors
-				.add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
-						getResourceLocator(), getString("_UI_Reference_name_feature"),
-						getString("_UI_PropertyDescriptor_description", "_UI_Reference_name_feature",
-								"_UI_Reference_type"),
-						DomainPackage.Literals.REFERENCE__NAME, true, false, false,
-						ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
-	}
-
-	/**
 	 * This adds a property descriptor for the Entity feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -138,6 +122,22 @@ public class ReferenceItemProvider extends ItemProviderAdapter implements IEditi
 								"_UI_Reference_type"),
 						DomainPackage.Literals.REFERENCE__LOWER_BOUND, true, false, false,
 						ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE, null, null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Name feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addNamePropertyDescriptor(Object object) {
+		itemPropertyDescriptors
+				.add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+						getResourceLocator(), getString("_UI_Reference_name_feature"),
+						getString("_UI_PropertyDescriptor_description", "_UI_Reference_name_feature",
+								"_UI_Reference_type"),
+						DomainPackage.Literals.REFERENCE__NAME, true, false, false,
+						ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
 	}
 
 	/**
